7d1028daff1795c929e80c4499ad9378ab7d38d6
Marcelina Kowalska <marcelina.kowalska@orange.com>
2024-01-04 14:21:47 +0100

    maybe working

--- main_2.py
+++ main_2.py
@@ -1,17 +1,15 @@
 import json
-import math
+import cmath
 import matplotlib.pyplot as plt
 import numpy as np
-
-
 
 with open('input.json', 'r') as file:
     data = json.load(file)
 
-L = 10 * 1e-3 # 10um w metrach
+L = 9 * 1e-4  # w metrach
 num_points = 1000
-start_value = 1.35e-6 #początkowy zakres fal
-end_value = 1.7e-6 #końcowy zakres fal
+start_value = 1.35e-6  # początkowy zakres fal
+end_value = 1.7e-6  # końcowy zakres fal
 wavelengths = np.linspace(start_value, end_value, num_points)
 
 for single_case in data:
@@ -21,18 +19,15 @@ for single_case in data:
     bragg_wavelength = 2 * n_eff * period
     final = []
     for index, single_point in enumerate(wavelengths):
-        ro = 2 * math.pi * n_eff * (1/single_point - 1/bragg_wavelength)
-        k = ro + 0.0001
-        gamma_b = math.sqrt(k**2 - ro**2)
-        reflectance = (k**2 * math.sinh(gamma_b * L)**2) / (ro**2 * math.sinh(gamma_b * L)**2 + gamma_b**2 * math.cosh(gamma_b * L)**2)
-        final.append(1 - reflectance)
-
+        ro = 2 * cmath.pi * n_eff * (1 / single_point - 1 / bragg_wavelength)
+        k = ro + 0.001
+        gamma_b = cmath.sqrt(k ** 2 - ro ** 2) if k ** 2 > ro ** 2 else 1j * cmath.sqrt(ro ** 2 - k ** 2)
+        reflectance = (k ** 2 * cmath.sinh(gamma_b * L) ** 2) / (
+                    ro ** 2 * cmath.sinh(gamma_b * L) ** 2 + gamma_b ** 2 * cmath.cosh(gamma_b * L) ** 2)
+        final.append(reflectance)
 
     plt.plot(wavelengths, final)
     plt.xlabel("Wavelength")
     plt.ylabel("Response")
     plt.grid(True)
     plt.show()
-
-
-
