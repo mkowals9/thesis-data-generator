34413202ad61d5cc22920e05d109888fed5bca18
Marcelina Kowalska <marcelina.kowalska@orange.com>
2024-02-13 19:19:19 +0100

    saving plots to gif file

--- display_data.py
+++ display_data.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import imageio
 
 
 def display_data(wavelengths, y_axis, delta_n_eff, n_eff, period, ylabel, title):
@@ -6,11 +7,23 @@ def display_data(wavelengths, y_axis, delta_n_eff, n_eff, period, ylabel, title)
     plt.xlabel("Wavelength")
     plt.ylabel(ylabel)
     plt.title(title)
-    stats = (f'delta_n_eff = {delta_n_eff:.2e}\n'
-             f'n_eff = {n_eff:.2f}\n'
-             f'period = {period:.2e}')
+    stats = (f'delta_n_eff = {delta_n_eff:.3e}\n'
+             f'n_eff = {n_eff:.4f}\n'
+             f'period = {period:.3}')
     bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.5)
-    plt.text(0.95, 1.1, stats, fontsize=10, bbox=bbox, transform=plt.gca().transAxes,
+    plt.text(0.75, 0.9, stats, fontsize=10, bbox=bbox, transform=plt.gca().transAxes,
              verticalalignment='top', horizontalalignment='left')
+    #plt.ylim(1e-16, 1e0)
+    #plt.yscale('log')
+    plt.ylim(0, 0.4)
     plt.grid(True)
-    plt.show()
+    #plt.show()
+    plt.savefig(f'./plots/plot_delta_n_eff {delta_n_eff:.2e}-n_eff {n_eff:.2f}-period {period:.2e}.png')
+    plt.clf()
+
+
+def save_plots_to_one_gif(filenames):
+    images = []
+    for filename in filenames:
+        images.append(imageio.imread('./plots/'+filename))
+    imageio.mimsave('plots_with_max_y.gif', images)
--- main_model.py
+++ main_model.py
@@ -1,7 +1,9 @@
 import json
 import cmath
+import random
 import numpy as np
-from display_data import display_data
+from display_data import display_data, save_plots_to_one_gif
+import os
 
 with open('input.json', 'r') as file:
     data = json.load(file)
@@ -14,6 +16,10 @@ wavelengths = np.linspace(start_value, end_value, num_points)
 fringe = 1
 
 model_data = []
+
+quarter_length = len(data) // 5
+random.shuffle(data)
+data = data[:quarter_length]
 
 for single_case in data:
     n_eff = single_case["n_eff"]
@@ -36,16 +42,22 @@ for single_case in data:
     title = "Reflectance - numerical model"
     display_data(wavelengths, final_reflectance, delta_n_eff, n_eff, period, ylabel, title)
     #
-    ylabel = "Transmittance"
-    title = "Transmittance - numerical model"
-    display_data(wavelengths, final_transmittance, delta_n_eff, n_eff, period, ylabel, title)
+    # ylabel = "Transmittance"
+    # title = "Transmittance - numerical model"
+    # display_data(wavelengths, final_transmittance, delta_n_eff, n_eff, period, ylabel, title)
 
-    model_data.append({
-        "reflectance": final_reflectance,
-        "delta_n_eff": delta_n_eff,
-        "n_eff": n_eff,
-        "period": period
-    })
+    # model_data.append({
+    #     "reflectance": final_reflectance,
+    #     "delta_n_eff": delta_n_eff,
+    #     "n_eff": n_eff,
+    #     "period": period
+    # })
 
-with open("data_model_input.json", "w") as outfile:
-    json.dump(model_data, outfile, indent=4)
+print("Creating gif")
+directory = './plots/'
+filenames = os.listdir(directory)
+
+save_plots_to_one_gif(filenames)
+
+# with open("data_model_input.json", "w") as outfile:
+#     json.dump(model_data, outfile, indent=4)
