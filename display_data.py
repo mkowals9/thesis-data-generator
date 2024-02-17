173529e9295285321d0aa1ff22f359f22f976fc5
Marcelina Kowalska <marcelina.kowalska@orange.com>
2024-02-17 16:45:22 +0100

    added X(z) as input param

--- display_data.py
+++ display_data.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
 import imageio
+import datetime
 
-
-def display_data(wavelengths, y_axis, delta_n_eff, n_eff, period, ylabel, title):
+def display_data(wavelengths, y_axis, delta_n_eff, n_eff, X_z, period, ylabel, title):
     plt.plot(wavelengths, y_axis)
     plt.xlabel("Wavelength")
     plt.ylabel(ylabel)
     plt.title(title)
     stats = (f'delta_n_eff = {delta_n_eff:.3e}\n'
              f'n_eff = {n_eff:.4f}\n'
-             f'period = {period:.3}')
+             f'period = {period:.3}\n'
+             f'X_z = {X_z:.5f}')
     bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.5)
     plt.text(0.75, 0.9, stats, fontsize=10, bbox=bbox, transform=plt.gca().transAxes,
              verticalalignment='top', horizontalalignment='left')
     #plt.ylim(1e-16, 1e0)
     #plt.yscale('log')
-    plt.ylim(0, 0.4)
+    plt.ylim(0, 0.6)
     plt.grid(True)
     #plt.show()
     plt.savefig(f'./plots/plot_delta_n_eff {delta_n_eff:.2e}-n_eff {n_eff:.2f}-period {period:.2e}.png')
@@ -26,4 +27,5 @@ def save_plots_to_one_gif(filenames):
     images = []
     for filename in filenames:
         images.append(imageio.imread('./plots/'+filename))
-    imageio.mimsave('plots_with_max_y.gif', images)
+    ct = datetime.datetime.now()
+    imageio.mimsave(f'./gifs/plots_{ct.timestamp()}.gif', images)
--- input_data_generator.py
+++ input_data_generator.py
@@ -14,26 +14,32 @@ min_grating_period = 535e-9
 max_grating_period = 540e-9
 grating_periods = np.linspace(min_grating_period, max_grating_period, other_params_points)
 
-min_X = 0.01
-max_X = 0.99
-X = random.uniform(min_X, max_X/2)
+# min_X = 0.01
+# max_X = 0.99
+# X = random.uniform(min_X, max_X/2)
 
 min_delta_n_eff = 1e-5
 max_delta_n_eff = 1e-4
 delta_n_effs = np.linspace(min_delta_n_eff, max_delta_n_eff, other_params_points)
 
-cartesian_product = itertools.product(n_effs, grating_periods, delta_n_effs)
+min_X_z = 0.01
+max_X_z = 0.99
+X_z_s = np.linspace(min_X_z, max_X_z, other_params_points)
+
+
+cartesian_product = itertools.product(n_effs, grating_periods, delta_n_effs, X_z_s)
 
 print("Saving to file")
 
 result = []
 for index, combination in enumerate(cartesian_product):
-    n_eff, grating_period, delta_n_eff = combination
+    n_eff, grating_period, delta_n_eff, X_z = combination
     result.append({
         "n_eff": n_eff,
         "grating_period": grating_period,
-        "delta_n_eff": delta_n_eff
+        "delta_n_eff": delta_n_eff,
+        "X_z": X_z
     })
 
-with open("input.json", "w") as outfile:
+with open("new_input.json", "w") as outfile:
     json.dump(result, outfile, indent=4)
